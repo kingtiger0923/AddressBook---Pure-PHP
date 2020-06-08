593a67770ca3cdd6efc44cf81ff6945bf38a2e51
kingtiger0923 <58518200+kingtiger0923@users.noreply.github.com>
2020-06-09 00:16:20 +0800

    complete

---
+++ _src/footer.php
@@ -0,0 +1,4 @@
+<div class="appFooter">
+  <h3>Developed by King Tiger</h3>
+  <h4 class="title w100">Only Using jQuery/CSS/HTML/PHP/Javascript</h4>
+</div>
--- _src/header.php
+++ _src/header.php
@@ -1,4 +1,3 @@
 <div class="appHeader">
   <h1 class="title w100">Address Book</h1>
-  <!-- <h4 class="title w100">I didn't use any other css or sources including Metrial-UI, Bootstrap ...</h4> -->
 </div>
